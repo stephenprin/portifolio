f9ba95da1725fb8a8e29c3717dea0a6b8864217a
Prince Nmezi <stephenprince427@gmail.com>
2023-04-28 22:05:00 +0100

    more project added

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import {projectData} from "../data"
+import {inprogress, projectData} from "../data"
 
 
 const Project = () => {
@@ -39,7 +39,37 @@ const Project = () => {
                   })}
                 
                 
-            </div>
+        </div>
+        <h2 className='progress-project'>Project in Progress</h2>
+        <div className="portifos">
+                 { inprogress.map((project) => {
+                      const { id, image, description, title,href1, href2 ,technology} = project;
+                      return (
+                        <div key={id} className="porti shadow shadow-blue-600/40 md:shadow-slate-600/40">
+                        <div className="thumbnail">
+                            <img src={image} alt="project"/>
+                        </div>
+                       
+                        <div>
+                                  <h3 className="text-cyan-600 text-[1.4rem] text-center mb-4">{ title}</h3>
+                        <hr className="w-[50px] h-[3px] bg-gradient-to-r from-cyan-500 via-cyan-100 to-cyan-800 m-auto"/>
+                                  <p className="text-sm px-4 text-slate-400 text-justify mt-3">{ description}</p>
+                             <div className="port-link mt-4 flex gap-4 ml-4 text-cyan-600 mb-8">
+                                <a className="text-[0.7rem]" href={href1}>GITHUB</a>
+                                <a className="text-[0.7rem] text" href={href2}>LIVE DEMO</a>
+                                      
+                                  </div>
+                                  <div className='p-4 technology'>
+                                      <span className="text-sm text-slate-400">Technologies used:</span>
+                                      <span className="text-sm text-cyan-600"> { technology}</span>
+                                  </div>
+                                 
+                        </div>
+                    </div>
+                      )
+                 })}
+          </div>
+
         </div>
      </section>
   )
--- src/data.js
+++ src/data.js
@@ -46,6 +46,7 @@ export const socialLinks = [
 ]
 
 export const projectData = [
+
     {
         id: 1,
         image: "https://res.cloudinary.com/dy27auwjb/image/upload/v1676027608/shoprite/smooze_qowtog.png",  
@@ -57,6 +58,15 @@ export const projectData = [
     },
     {
         id: 2,
+        image: "https://res.cloudinary.com/dy27auwjb/image/upload/v1682710831/Screenshot_2023-04-28_at_20.33.41_dw2pyb.png",  
+        title: "Swift Rider",
+        description: "Swift Rider is a web application designed for ride-sharing services, allowing drivers to quickly and easily find nearby passengers and vice versa. The app includes features such as real-time GPS tracking, in-app messaging, and payment processing",
+        href1: "https://github.com/",
+        href2: "https://swift-rider.netlify.com/",
+        technology: "React, CSS, JavaScript, NodeJS,Express, Typescript, postgresql, swagger, testing, CI/CD"
+    },
+    {
+        id: 3,
         image: "https://res.cloudinary.com/dy27auwjb/image/upload/v1676027960/shoprite/gitsearcher_yoajfz.png",  
         title: "Github User Search",
         description: "An application that allows users to search for a specific GitHub user by username and retrieve information about their profile and activity on the platform. This information can include the user's bio, location, email, number of followers, repositories, and recent activity such as pull requests and comments. The purpose of a GitHub user searcher is to help developers,recruiters, and others to quickly find and gather information about a specific GitHub user.",
@@ -65,7 +75,7 @@ export const projectData = [
         technology: "React, Tailwind CSS, React Router, React Hooks, Context API, GitHub API, FusionCharts, CSS"
     },
     {
-        id: 3,
+        id: 4,
         image: "https://res.cloudinary.com/dy27auwjb/image/upload/v1676027607/shoprite/project3_gzc4av.png",
         title: "Admin sales Dashboard",
         description: "An interface designed to provide an overview of sales performance and activity for a business. It typically displays sales data in the form of charts, graphs, and tables, allowing administrators to quickly and easily monitor key performance metrics.The ultimate goal of an admin sales dashboard is to provide business owners and administrators with real-time visibility into their sales performance, enabling them to make data-driven decisions to improve their bottom line.",
@@ -82,6 +92,21 @@ export const projectData = [
         href2: "https://sparkly-treacle-fc57e2.netlify.app/",
         technology: "React, Tailwind CSS, React Router, React Hooks, Context API, Unsplash API, CSS"
     }
+    
+]
+
+export const inprogress = [
+    
+        {
+            id: 1,
+            image: "https://res.cloudinary.com/dy27auwjb/image/upload/v1682715422/Screenshot_2023-04-28_at_21.56.21_bekeys.png",  
+            title: "Neflix Clone",
+            description: "A Netflix clone is a web application that mimics the features and functionality of the popular streaming service Netflix. A Netflix clone allows users to sign up, log in, browse and search for movies and TV shows, view details about individual titles, add titles to their watchlist, and stream content on demand.",
+            href1: "",
+            href2: "https://github.com/stephenprin/netflix_clone",
+            technology: "Stripe checkout,React, Webhooks, Firebase, Redux, React Router,Google Auth,React Hooks,  CSS"
+        },
+    
 ]
 
 export const skills = [
